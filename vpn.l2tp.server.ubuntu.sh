891ed5574d9f55d7692a8484cbbe715bf80ede13
greenSnot <greenSnot@users.noreply.github.com>
2019-04-26 13:24:51 -0400

    vpn l2tp server

---
+++ vpn.l2tp.server.ubuntu.sh
@@ -0,0 +1,5 @@
+wget https://git.io/vpnsetup -O vpnsetup.sh && sudo \
+VPN_IPSEC_PSK='your_ipsec_pre_shared_key' \
+VPN_USER='your_vpn_username' \
+VPN_PASSWORD='your_vpn_password' \
+sh vpnsetup.sh
